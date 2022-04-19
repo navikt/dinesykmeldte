897d3ba1ed4d899382346969fd6b82b56d66623d
Karl J. Overå <k@rl.run>
2022-04-19 10:49:52 +0200

    don't show arbeidsforEtterPeriode when null

--- src/pages/sykmeldt/[sykmeldtId]/soknad/[soknadId].page.tsx
+++ src/pages/sykmeldt/[sykmeldtId]/soknad/[soknadId].page.tsx
@@ -52,7 +52,7 @@ function SoknadIdPage(): JSX.Element {
             }}
         >
             <Head>
-                <title>Søknad for for {sykmeldtName} - nav.no</title>
+                <title>Søknad for {sykmeldtName} - nav.no</title>
             </Head>
             <SideNavigation sykmeldt={sykmeldtQuery.sykmeldt}>
                 <ContentContainer>
