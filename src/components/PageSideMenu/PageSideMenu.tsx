f9c15e5b8fce255822c0c0666bb126ce05cd78d8
Karl J. Overå <k@rl.run>
2022-09-19 09:25:18 +0200

    disable notifications

--- src/components/PageSideMenu/PageSideMenu.tsx
+++ src/components/PageSideMenu/PageSideMenu.tsx
@@ -2,7 +2,6 @@ import { Pages, SideMenu } from '@navikt/dinesykmeldte-sidemeny';
 import Link from 'next/link';
 
 import { PreviewSykmeldtFragment } from '../../graphql/queries/graphql.generated';
-import { isPreviewSoknadNotification } from '../../utils/soknadUtils';
 
 type Props = {
     sykmeldt: PreviewSykmeldtFragment | null;
@@ -19,7 +18,9 @@ function PageSideMenu({ sykmeldt, activePage }: Props): JSX.Element | null {
             activePage={activePage}
             routes={{
                 Sykmeldinger: {
-                    notifications: sykmeldt.sykmeldinger.filter((it) => !it.lest).length,
+                    // TODO: Notifications are disabled for all routes until eSyfo decides how they want to handle notifications
+                    // notifications: sykmeldt.sykmeldinger.filter((it) => !it.lest).length,
+                    notifications: 0,
                     internalRoute: ({ children, ...rest }) => (
                         <Link href={`/sykmeldt/${sykmeldt.narmestelederId}/sykmeldinger`} passHref>
                             <a {...rest}>{children}</a>
@@ -27,7 +28,8 @@ function PageSideMenu({ sykmeldt, activePage }: Props): JSX.Element | null {
                     ),
                 },
                 Soknader: {
-                    notifications: sykmeldt.previewSoknader.filter((it) => isPreviewSoknadNotification(it)).length,
+                    // notifications: sykmeldt.previewSoknader.filter((it) => isPreviewSoknadNotification(it)).length,
+                    notifications: 0,
                     internalRoute: ({ children, ...rest }) => (
                         <Link href={`/sykmeldt/${sykmeldt.narmestelederId}/soknader`} passHref>
                             <a {...rest}>{children}</a>
@@ -36,15 +38,18 @@ function PageSideMenu({ sykmeldt, activePage }: Props): JSX.Element | null {
                 },
                 Meldinger: {
                     hide: sykmeldt.aktivitetsvarsler.length === 0,
-                    notifications: sykmeldt.aktivitetsvarsler.filter((it) => !it.lest).length,
+                    // notifications: sykmeldt.aktivitetsvarsler.filter((it) => !it.lest).length,
+                    notifications: 0,
                     internalRoute: ({ children, ...rest }) => (
                         <Link href={`/sykmeldt/${sykmeldt.narmestelederId}/meldinger`} passHref>
                             <a {...rest}>{children}</a>
                         </Link>
                     ),
                 },
-                Dialogmoter: sykmeldt.dialogmoter.length,
-                Oppfolgingsplaner: sykmeldt.oppfolgingsplaner.length,
+                // Dialogmoter: sykmeldt.dialogmoter.length,
+                Dialogmoter: 0,
+                // Oppfolgingsplaner: sykmeldt.oppfolgingsplaner.length,
+                Oppfolgingsplaner: 0,
                 DineSykmeldte: {
                     notifications: 0,
                     internalRoute: ({ children, ...rest }) => (
