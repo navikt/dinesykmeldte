313696127f1f04f913dd2e4854a62be4a44738f8
Karl J. Overå <k@rl.run>
2024-09-23 08:12:15 +0200

    fix: dont crash on 0% gradert

--- src/components/soknadpanel/SoknadPerioder.test.tsx
+++ src/components/soknadpanel/SoknadPerioder.test.tsx
@@ -25,4 +25,24 @@ describe('SoknadPerioder', () => {
         expect(screen.getByText('1. - 15. januar 2022')).toBeInTheDocument()
         expect(screen.getByText('80% sykmeldt i 15 dager')).toBeInTheDocument()
     })
+
+    it('Should not crash on gradert with 0%', () => {
+        render(
+            <SoknadPerioder
+                perioder={[
+                    {
+                        __typename: 'Soknadsperiode',
+                        fom: '2022-01-01',
+                        tom: '2022-01-15',
+                        sykmeldingstype: PeriodeEnum.Gradert,
+                        sykmeldingsgrad: 0,
+                    },
+                ]}
+            />,
+        )
+
+        expect(screen.getByRole('heading', { name: 'Perioden det gjelder (f.o.m. - t.o.m.)' })).toBeInTheDocument()
+        expect(screen.getByText('1. - 15. januar 2022')).toBeInTheDocument()
+        expect(screen.getByText('0% sykmeldt i 15 dager')).toBeInTheDocument()
+    })
 })
